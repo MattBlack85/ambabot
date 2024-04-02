19890dbb90c10eb328a80089f5595889c3a09ade
Mattia Procopio <promat85@gmail.com>
2024-04-02 13:43:56 +0200

    Add explicit waits for chrome/ff

--- src/bot.py
+++ src/bot.py
@@ -9,8 +9,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 from fake_useragent import UserAgent
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from telegram import Bot
 
 load_dotenv()
@@ -37,13 +40,13 @@ BUSY_MODAL_ID = "jconfirm-box53013"
 logger = logging.getLogger("ambabot")
 here = Path(__file__)
 logfile_path = here.parent.parent / "ambabot.log"
-logging.basicConfig(
-    format="%(asctime)s %(message)s",
-    filename=logfile_path,
-    filemode="a",
-    encoding="utf-8",
-    level=logging.WARN,
-)
+# logging.basicConfig(
+#     format="%(asctime)s %(message)s",
+#     filename=logfile_path,
+#     filemode="a",
+#     encoding="utf-8",
+#     level=logging.WARN,
+# )
 
 
 def generate_random_wait_times(min_secs: int, max_secs: int) -> float:
@@ -54,7 +57,7 @@ class Driverfactory:
     def _setup_ff_driver():
         ff_opts = webdriver.FirefoxOptions()
         ff_opts.set_preference("general.useragent.override", USER_AGENT)
-        ff_opts.add_argument("--headless")
+        # ff_opts.add_argument("--headless")
 
         if platform.machine() == "aarch64":
             service = webdriver.FirefoxService(executable_path="/usr/bin/geckodriver")
@@ -67,7 +70,7 @@ class Driverfactory:
 
     def _setup_chrome_driver():
         chrome_opts = webdriver.ChromeOptions()
-        chrome_opts.add_argument("--headless=new")
+        # chrome_opts.add_argument("--headless=new")
         chrome_opts.add_argument(f"--user-agent={USER_AGENT}")
         driver = webdriver.Chrome(chrome_opts)
 
@@ -107,18 +110,27 @@ def main(driver, loop, bot: Bot):
     book_passport = driver.find_element(By.XPATH, '//a[@href="/Services/Booking/73"]')
     book_passport.send_keys(Keys.ENTER)
 
-    time.sleep(generate_random_wait_times(9, 10))
+    if driver.name == "firefox":
+        time.sleep(generate_random_wait_times(4, 6))
+        wait = WebDriverWait(driver, 15)
+    else:
+        wait = WebDriverWait(driver, 1)
 
     try:
-        driver.find_element(By.ID, BUSY_MODAL_ID)
+        wait.until(EC.presence_of_element_located((By.ID, BUSY_MODAL_ID)))
         for user_id in USER_IDS:
             loop.run_until_complete(bot.send_message(user_id, "Passports slots are available! Login and book one"))
         logger.warning("Passports slots are available! Login and book one")
-    except:  # NOQA
+    except TimeoutException:
+        logger.warning("The page doesn't load")
+    except Exception as e:  # NOQA
         logger.warning(f"No available passports slots - user-agent: {USER_AGENT} - driver: {driver.name}")
 
-    logout = driver.find_element(By.XPATH, "//form[@id='logoutForm']/button[1]")
-    logout.submit()
+    try:
+        logout = driver.find_element(By.XPATH, "//form[@id='logoutForm']/button[1]")
+        logout.submit()
+    except NoSuchElementException:
+        pass
 
     driver.close()
 
